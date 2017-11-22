b59f7041abe177c1abe5482207950ab2be3b54f0
Piotr Majcher <piotr.majcher94@gmail.com>
2017-11-22 15:20:37 +0100

    Added travis.yml

--- app/src/main/java/com/piotrmajcher/piwind/piwindmobile/config/REST.java
+++ app/src/main/java/com/piotrmajcher/piwind/piwindmobile/config/REST.java
@@ -2,8 +2,9 @@ package com.piotrmajcher.piwind.piwindmobile.config;
 
 public interface REST {
 //    String BASE_URL = "http://192.168.0.110:8080";
-    String BASE_URL = "http://10.38.250.59:8080";
+//    String BASE_URL = "http://10.38.250.59:8080";
 //    String BASE_URL = "http://10.0.2.2:8080";
+    String BASE_URL = "https://piwind.herokuapp.com";
     String GET_STATIONS_LIST_ENDPOINT = "/stations/all";
     String STATISTIC_ENDPOINT = "/statistics";
     String REGISTER_USER_URL = "/register-user";
